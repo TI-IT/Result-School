d0a8da3b618ceaa64071ca2370333577dbb6843f
TIGRA <go.tiit.ogle@gmail.com>
2023-03-06 22:41:38 +0300

    03

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('#modal');
 const content = document.querySelector('#content');
 const backdrop = document.querySelector('#backdrop');
+const progress = document.querySelector('#progress');
 
 content.addEventListener('click', openCard);
 backdrop.addEventListener('click', closeModal);
@@ -17,4 +18,58 @@ function closeModal() {
   console.log(modal.classList.value);
 }
 
-console.log(modal);
+const technologies = [
+  {
+    id: 1,
+    title: 'HTML',
+    description: 'Html text',
+    type: 'html',
+    done: true,
+  },
+  {
+    id: 2,
+    title: 'CSS',
+    description: 'CSS text',
+    type: 'css',
+    done: false,
+  },
+  {
+    id: 3,
+    title: 'JS',
+    description: 'JS text',
+    type: 'js',
+    done: false,
+  },
+  {
+    id: 4,
+    title: 'REACT',
+    description: 'REACT text',
+    type: 'react',
+    done: false,
+  },
+];
+
+function init() {
+  if (technologies.length === 0) {
+    content.innerHTML = '<p class="emty">Технологий пока нету, добавьте</p>';
+  } else {
+    // let html = '';
+    // for (let i = 0; i < technologies.length; i++) {
+    //   const tech = technologies[i];
+    //   html += toCard(tech);
+    // }
+    // content.innerHTML = html;
+    content.innerHTML = technologies.map(toCard).join('');
+  }
+}
+
+function toCard(tech) {
+  const doneClass = tech.done ? 'done' : '';
+  return `
+<div class="card ${doneClass}">
+    <h3>${tech.title}</h3>
+</div>
+`;
+}
+
+init();
